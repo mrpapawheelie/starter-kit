9990b02a03af934d61fb4a374d6f1b1ae046e21d
P A P A  W H E E L I E <86974690+mrpapawheelie@users.noreply.github.com>
2023-11-30 07:45:00 -0700

    Update next.config.js
    add blog rewrite

--- packages/blog-starter-kit/themes/enterprise/next.config.js
+++ packages/blog-starter-kit/themes/enterprise/next.config.js
@@ -40,6 +40,14 @@ const config = {
 				source: '/api/collect',
 				destination: `${ADVANCED_ANALYTICS_BASE_URL}/api/collect`,
 			},
+      {
+        source: "/blog",
+        destination: "https://starter-kit-rose-seven.vercel.app/blog", -> Replace https://starter-kit-rose-seven.vercel.app with your own Vercel deployment URL from step 1
+      },
+      {
+        source: "/blog/:path*",
+        destination: "https://starter-kit-rose-seven.vercel.app/blog/:path*", -> Replace https://starter-kit-rose-seven.vercel.app with your own Vercel deployment URL from step 1
+      },
 		];
 	},
 };
